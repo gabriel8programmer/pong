5e26ee03ee81b1f56a660e1a52669c4710b52126
gabriel8programmer <gabriel8github@gmail.com>
2024-02-27 15:13:21 -0300

    update code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,61 @@
-
 class Game {
-
-    constructor(canvas){
-        //get width and height of the screen
+    constructor(canvas) {
+        // Obtendo largura e altura do canvas
         this.width = canvas.width;
         this.height = canvas.height;
+        this.tileSize = 20;
 
-        //get context 2d
+        // Obtendo contexto 2D
         this.ctx = canvas.getContext("2d");
 
-        //state of the game
-        this.running = false;
+        // Estado do jogo
+        this.running = true;
     }
 
-    init = ()=> {
+    init() {
+        // Inicializando a bola
+        this.ball = {
+            x: this.width / 2 - this.tileSize / 2,
+            y: this.height / 2 - this.tileSize / 2,
+            w: this.tileSize,
+            h: this.tileSize,
+            c: "#ff0", // Cor amarela
+
+            draw: () => {
+                // Atualizando cor do contexto
+                this.ctx.fillStyle = this.ball.c; // Ajuste aqui
+                this.ctx.fillRect(this.ball.x, this.ball.y, this.ball.w, this.ball.h); // Ajuste aqui
+            }
+        };
+
         this.run();
     }
 
-    drawRect = (x, y, w, h)=> {
-        //update screen color
+    drawRect(x, y, w, h, c) {
+        // Atualizando cor do contexto
+        this.ctx.fillStyle = c;
         this.ctx.fillRect(x, y, w, h);
     }
 
-    render = ()=> {
-        //update screen color
-        this.drawRect(0, 0, this.width, this.height);
+    render() {
+        // Atualizando cor do contexto
+        this.drawRect(0, 0, this.width, this.height, "#000"); // Cor de fundo preta
+
+        // Desenhando a bola
+        this.ball.draw(); // Não é necessário passar o contexto aqui, pois já está definido no método draw da bola
     }
 
-    update = ()=> {
-        console.log("Updating");
+    update() {
+        console.log("Atualizando");
     }
 
-    run = ()=> {
+    run() {
         this.render();
-        if (this.running){
+        if (this.running) {
             this.update();
         }
-        requestAnimationFrame(this.run.bind(this));
+        requestAnimationFrame(this.run.bind(this)); // Garante que a função run seja chamada no contexto adequado
     }
-
 }
 
 export default Game;
